92550f53141841292aa1e58603067d722bbed19e
korytkoBT <korytko07@gmail.com>
2015-08-23 19:39:52 +0200

    Local Copy

---
+++ REST/REST/Models/Artist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST.Models
+{
+  public class Artist
+  {
+    public int ID  { get; set; }
+    public string Name { get; set; }
+    public string ActivityYears { get; set; }
+
+    public Artist()
+    {
+      this.Songs = new HashSet<Song>();
+    }
+
+    public virtual ICollection<Song> Songs { get; set; } 
+  }
+}
